57541521869d9b211fb79062cfae4e5b0ed84321
Yan <you@example.com>
2019-11-22 13:34:50 -0300

    dados da classe despesa guardados num array

--- semana4/aula4-funcoes-de-array/controlador-de-despesas/despesas.js
+++ semana4/aula4-funcoes-de-array/controlador-de-despesas/despesas.js
@@ -4,3 +4,25 @@ function apagaForm(){
     document.getElementById('valorMax').value = ""
     document.getElementById('valorMin').value = ""
 }
+
+class despesa{
+    constructor(valor, descricao, tipo){
+        this.valor = valor
+        this.descricao = descricao
+        this.tipo = tipo
+    }
+}
+
+const arrayDespesa = []
+
+function salvar(){
+    console.log("Teste Salvar")
+    const valor = document.getElementById('valor').value
+    const descricao = document.getElementById('descricao').value
+    const tipo = document.getElementById('tipoDespesa').value
+
+    const dadosCadastrados = new despesa(valor, descricao, tipo)
+    console.log(dadosCadastrados)
+    arrayDespesa.push(dadosCadastrados)
+    console.log(arrayDespesa)
+}
