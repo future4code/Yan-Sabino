0e4f7f4268caa02f4cb9403264b51f1483ba59b0
Yan <you@example.com>
2019-11-22 16:47:15 -0300

    Entrega não finalizada

--- semana4/aula4-funcoes-de-array/controlador-de-despesas/despesas.js
+++ semana4/aula4-funcoes-de-array/controlador-de-despesas/despesas.js
@@ -1,10 +1,3 @@
-function apagaForm(){
-    document.getElementById('valor').value = ""
-    document.getElementById('descricao').value = ""
-    document.getElementById('valorMax').value = ""
-    document.getElementById('valorMin').value = ""
-}
-
 class despesa{
     constructor(valor, descricao, tipo){
         this.valor = valor
@@ -17,24 +10,44 @@ const arrayDespesa = []
 
 function salvar(){
     console.log("Teste Salvar")
-    
-    const valor = document.getElementById('valor').value
+    const valor = document.getElementById("valor").value
+    console.log(valor)
     const descricao = document.getElementById('descricao').value
     const tipo = document.getElementById('tipoDespesa').value
 
+    
     if(valor === "" || descricao === "" || tipo === "none"){
         alert("Por favor preencha os campos!")
         document.getElementById("valor").focus()
-        document.getElementById("descricao").focus()
-        document.getElementById("tipoDespesa").focus()
         return false
+        }else if(valor !== "" || descricao !== "" || tipo !== "none"){
+                    const dadosCadastrados = new despesa(valor, descricao, tipo)
+                    console.log(dadosCadastrados)
+                    arrayDespesa.push(dadosCadastrados)
+                    console.log(arrayDespesa)
         }
+    }        
+        
 
-    const dadosCadastrados = new despesa(valor, descricao, tipo)
-    console.log(dadosCadastrados)
-    arrayDespesa.push(dadosCadastrados)
-    console.log(arrayDespesa)
 
-    
-
+function apagaForm(){
+    document.getElementById('valor').value = ""
+    document.getElementById('descricao').value = ""
+    document.getElementById('valorMax').value = ""
+    document.getElementById('valorMin').value = ""
 }
+
+
+function addDespesa(){
+    const divAddDespesa = document.getElementById("valoresCadastrados")
+    divAddDespesa.innerHTML = ""
+    for(const post of arrayDespesa){
+        divAddDespesa.innerHTML += criaDespesa(post)
+    }
+}
+
+function criaDespesa(post){
+    return "<ul><li>Descrição: " + post.descricao + "</li>" +
+    "<li>Tipo: " + post.tipo + "</li>" + "<li>Valor: " + post.valor +
+     "</li></ul>"
+    }   
