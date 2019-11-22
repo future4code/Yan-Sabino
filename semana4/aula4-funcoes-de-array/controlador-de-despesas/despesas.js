3773d0fa8a0349fbd67435af39a73b3c4b7d0753
Yan <you@example.com>
2019-11-22 13:17:43 -0300

    funcao apagaForm ativa para todos os campos

---
+++ semana4/aula4-funcoes-de-array/controlador-de-despesas/despesas.js
@@ -0,0 +1,6 @@
+function apagaForm(){
+    document.getElementById('valor').value = ""
+    document.getElementById('descricao').value = ""
+    document.getElementById('valorMax').value = ""
+    document.getElementById('valorMin').value = ""
+}
