92f730c326c2b7654db5bd80c1b287a51493958a
Yan <you@example.com>
2019-11-26 16:27:50 -0300

    CvButtom Feito

--- semana5/aula2-componentes/cv-yan-futurefour/cv-yanfuturefour/src/App.js
+++ semana5/aula2-componentes/cv-yan-futurefour/cv-yanfuturefour/src/App.js
@@ -4,6 +4,7 @@ import CvFlex from './Components/CvFlex/CvFlex';
 import CvFlexTitulo from './Components/CvFlexTitulo/CvFlexTitulo'
 import CvFlexDescricao from './Components/CvFlexDescricao/CvFlexDescricao'
 import CvInfos from './Components/CvInfos/CvInfos'
+import CvButtom from './Components/CvButtom/CvButtom'
 
 function App() {
   return (
@@ -17,7 +18,10 @@ function App() {
                 internacional, proatividade e trabalho em equipe. Inglês
                 Avançado, comunicando-se em espanhol e francês." />
         <CvInfos imagem="https://encurtador.com.br/jEHX4" label="E-mail:" texto="yan.sabino@hotmail.com" />
-        <CvInfos imagem="https://encurtador.com.br/kmCQ1" label="Endereço:" texto="Rua dos Sabias, 1234"/> 
+        <CvInfos imagem="https://encurtador.com.br/kmCQ1" label="Endereço:" texto="Rua dos Sabias, 1234"/>
+        <CvButtom imagem="https://encurtador.com.br/hmxDX" texto="Ver Mais"  />
+        <CvFlexTitulo titulo="Experiências Profissionais"/>
+        
     </CvFlex>
   );
 }
