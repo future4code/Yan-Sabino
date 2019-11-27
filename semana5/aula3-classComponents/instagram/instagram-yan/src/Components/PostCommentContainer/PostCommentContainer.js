b921c44e43d2c3d578bc82a36f6d50349657c28a
Yan <you@example.com>
2019-11-27 19:18:03 -0300

    Entrega Completa

---
+++ semana5/aula3-classComponents/instagram/instagram-yan/src/Components/PostCommentContainer/PostCommentContainer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './PostCommentContainer.css'
+
+class PostCommentContainer extends React.Component{
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return(
+			<div className="PostCommentContainer">
+			
+				
+			</div>
+        )
+	}
+
+}
+export default PostCommentContainer
--- semana5/aula3-classComponents/instagram/instagram-yan/src/Components/PostLikeAndComent/PostLikeAndComent.js
+++ semana5/aula3-classComponents/instagram/instagram-yan/src/Components/PostLikeAndComent/PostLikeAndComent.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import './PostLikeAndComent.css'
 
+function ContainerParaMostrar(mostra) {
+    if (mostra) {
+      return <div id="PostCommentContainer">
+      <input placeholder="Escreva Seu Comentário"></input>
+      {/* <button onClick={this.addComment}>Comentar</button> */}
+     </div>;
+    }
+  }
 
 class PostLikeAndComent extends React.Component{
 	constructor(props){
         super(props);
         this.state={
+            mostrandoOContainer: false,
+            quantidadeComment: 0,
             quantidadeLike: 0,
             urlDoIcone: "https://encurtador.com.br/gGILT",
             urlDoComent: "https://encurtador.com.br/tIJV4"
@@ -28,22 +38,49 @@ class PostLikeAndComent extends React.Component{
     }
 
     aoClicarNoComment = () => {
+        // console.log("oi")
+        if(this.state.mostrandoOContainer){
+            this.setState({
+                mostrandoOContainer: false
+            })
+        }else{
+            this.setState({
+                mostrandoOContainer: true
+            })
+        }
+    }
+
+    addComment = () =>{
+        const numeroDeComentarios = this.state.quantidadeComment
+        this.setState({
+            quantidadeComment: numeroDeComentarios + 1
+        })
         
     }
 
 	render(){
 		return(
+            <div>
 			<div className="PostLikeAndComentContainer">
+                
                 <div id="container1">
                    <img alt="img" src={this.state.urlDoIcone} onClick={this.aoClicarNoIcone}/>
                    <p>{this.state.quantidadeLike}</p> 
                 </div>
 				<div id="container2">
-                    <img src={this.state.urlDoComent}/>
+                    <img src={this.state.urlDoComent} onClick={this.aoClicarNoComment}/>
+                    <p>{this.state.quantidadeComment}</p>
+                    
                 </div>
 			</div>
+            <div id="ContainerComments">
+                {ContainerParaMostrar(this.state.mostrandoOContainer)}
+                <button  onClick={this.addComment}>Comentar</button>
+            </div>
+            </div>
         )
 	}
 
 }
+
 export default PostLikeAndComent
