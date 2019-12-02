cdf7edb666301e2ea6e497ff1507371f7a60e8af
Your Name <you@example.com>
2019-12-02 16:51:37 -0300

    Mudanças mínimas2

--- semana6/aula1/form-yan/src/Components/Etapa1/Etapa1.js
+++ semana6/aula1/form-yan/src/Components/Etapa1/Etapa1.js
@@ -10,7 +10,28 @@ width:400px;
 `
 
 class Etapa1 extends React.Component{
-    
+    constructor(props){
+        super(props)
+        this.state = {
+            selectOption: "Ensino Médio Incompleto"
+        }
+    }
+
+    //Inicio da Primeira parte do desafio
+// alterarSelect = e =>{
+//     this.setState({
+//         selectOption: e.target.value
+//     })
+// }
+
+// selecionarPaginaSelect = () => {
+//     if(this.state.selectOption.value === "Ensino Médio Completo" || this.state.selectOption.value === "Ensino Superior Completo"){
+//        return <Etapa2/> 
+//     } else{
+//         return <Etapa3/>
+//     }
+// }
+
     render(){
         return(
             <ContainerEtapa1>
@@ -22,11 +43,11 @@ class Etapa1 extends React.Component{
             <p>3. Qual seu email?</p>
             <input></input>
             <p>4. Qual a sua escolaridade?</p>
-            <select>
-                <option>Ensino Médio Incompleto</option>
-                <option>Ensino Médio Completo</option>
-                <option>Ensino Superior Incompleto</option>
-                <option>Ensino Superior Completo</option>
+            <select value={this.state.selectOption} onChange={this.alterarSelect}>
+                <option value="Ensino Médio Incompleto">Ensino Médio Incompleto</option>
+                <option value="Ensino Médio Completo">Ensino Médio Completo</option>
+                <option value="Ensino Superior Incompleto">Ensino Superior Incompleto</option>
+                <option value="Ensino Médio Completo">Ensino Superior Completo</option>
             </select>
             
             </ContainerEtapa1>
