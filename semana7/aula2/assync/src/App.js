1133bc5be5676f2678dc22fb54136bfb02e4991e
Your Name <you@example.com>
2019-12-10 16:28:44 -0300

    conferencia Soter

--- semana7/aula2/assync/src/App.js
+++ semana7/aula2/assync/src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const token = "yan"
+const baseUrl = "https://us-central1-future4-users.cloudfunctions.net/api"
+
+ class App extends React.Component {
+    constructor(props){
+      super(props)
+
+      this.state = {
+        allUsers: []
+      }
+    }
+
+    getAllUsers = () => {
+      const url = `${baseUrl}/users/getAllUsers`
+      
+
+      const request = axios.get(url, {
+        headers: {
+          "api-token": token,
+        }
+      })
+
+      request.then((response)=>{
+        
+        console.log(response.data)
+        const allUsers = response.data.result.list
+        this.setState({ allUsers: allUsers });
+      })
+
+    }
+
+    componentDidMount(){
+        this.getAllUsers()
+    }
+
+
+    render(){
+      return (
+        <div>
+          
+        </div>
+      );
+
+    }
+  
 }
 
 export default App;
