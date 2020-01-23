ba6891e27cb89ced5c1b42fe88c742df893a6dd4
Yan <57139085+yansabino@users.noreply.github.com>
2020-01-23 14:30:23 -0300

    Update login.js

--- semana10/astrotrips-boilerplate/src/actions/login.js
+++ semana10/astrotrips-boilerplate/src/actions/login.js
@@ -10,7 +10,7 @@ export const login = (email, password) => async (dispatch) => {
         password,
     }
 
-    console.log(loginInformation)
+    
     try{
         const response = await axios.post(`${baseURL}/login`, loginInformation)
 
