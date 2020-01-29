ee11c7565109a3bc3492d18d53206cf2d5dd67eb
Yan Sabino <yan.sabino@hotmail.com>
2020-01-28 21:49:41 -0300

    tentei entender os testes, mas não entrou na minha cabeça

---
+++ semana12/4eddit-bouman-5/src/actions/index.test.js
@@ -0,0 +1,8 @@
+import {setPostAction} from './index'
+
+
+describe("teste actions posts", ()=>{
+    test("teste getActions", ()=>{
+       const action = setPostAction("setar os posts")
+    })
+})
