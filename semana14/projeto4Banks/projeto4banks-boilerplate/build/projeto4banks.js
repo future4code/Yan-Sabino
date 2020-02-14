7df98983b32721e7eb1f0ad7a0f67858151a159a
Yan Sabino <yan.sabino@hotmail.com>
2020-02-14 14:28:16 -0300

    adding the users without expenses

--- semana14/projeto4Banks/projeto4banks-boilerplate/build/projeto4banks.js
+++ semana14/projeto4Banks/projeto4banks-boilerplate/build/projeto4banks.js
@@ -1,10 +1,47 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const moment = require("moment");
+const fs_1 = require("fs");
 moment.locale("pt-br");
 const jsonFileName = "projeto4banks.json";
 const userName = process.argv[4];
 const userCpf = Number(process.argv[5]);
-const userBirthday = moment(process.argv[6], "DD/MM/YYYY HH:mm");
+const userBirthday = moment(process.argv[6], "DD/MM/YYYY");
 const userBalance = Number(process.argv[7]);
+const createAccounts = () => __awaiter(void 0, void 0, void 0, function* () {
+    const readAccountsPromise = new Promise((resolve, reject) => {
+        fs_1.readFile(jsonFileName, (err, data) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(data.toString());
+        });
+    });
+    const jsonContent = yield readAccountsPromise;
+    console.log(jsonContent);
+    const accountsObject = JSON.parse(jsonContent);
+    accountsObject.accountList.push({
+        user: {
+            name: userName,
+            cpfNumber: userCpf,
+            birthday: userBirthday.format("DD/MM/YYYY"),
+        },
+        balance: userBalance,
+    });
+    console.log(accountsObject);
+    fs_1.writeFileSync(jsonFileName, JSON.stringify(accountsObject));
+});
+createAccounts()
+    .then((accountsObject) => console.log(accountsObject.accountList))
+    .catch(err => console.error(err));
 //# sourceMappingURL=projeto4banks.js.map
--- semana14/projeto4Banks/projeto4banks-boilerplate/src/projeto4banks.ts
+++ semana14/projeto4Banks/projeto4banks-boilerplate/src/projeto4banks.ts
@@ -1,23 +1,68 @@
 import * as moment from "moment";
 import { readFile, writeFileSync } from "fs";
+
 moment.locale("pt-br");
 
-const jsonFileName: string = "projeto4banks.json"
-const userName: string = process.argv[4]
+const jsonFileName: string = "projeto4banks.json";
+const userName: string = process.argv[4];
 const userCpf: number = Number(process.argv[5])
-const userBirthday: any = moment(process.argv[6], "DD/MM/YYYY HH:mm")
+const userBirthday: any = moment(process.argv[6], "DD/MM/YYYY")
+// const userExpense: number[] = Number(process.argv[7])
+// const userExpenseValue: number = Number(process.argv[8])
+// const userExpenseDescription: string = process.argv[9]
+// const userExpenseDate: any = moment(process.argv[10], "DD/MM/YYYY")
 const userBalance: number = Number(process.argv[7])
-// const userExtract: number[] = Number(process.argv[8])
 
+type expense = {
+  value: number;
+  description: string;
+  date: Date;
+};
 
 type account = {
-    name: string;
-    cpfNumber: number;
-    birthday: Date;
-    balance: number;
-    extract: number[]
-}
+  user: {
+    name: string,
+    cpfNumber: number,
+    birthday: Date,
+  },
+//   expense: expense[],
+  balance: number,
+};
 
 type accountFile = {
-    accountList: account[]
-}
+  accountList: account[];
+};
+
+const createAccounts = async () => {
+  const readAccountsPromise: Promise<string> = new Promise(
+    (resolve, reject) => {
+      readFile(jsonFileName, (err: Error, data: Buffer) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        // console.log(data)
+        resolve(data.toString());
+      });
+    }
+  );
+
+  const jsonContent: string = await readAccountsPromise;
+  console.log(jsonContent);
+  const accountsObject: accountFile = JSON.parse(jsonContent);
+  accountsObject.accountList.push({
+    user: {
+        name: userName,
+        cpfNumber: userCpf,
+        birthday: userBirthday.format("DD/MM/YYYY"),
+      },
+    // expense: userExpense,
+    balance: userBalance,
+  });
+  console.log(accountsObject);
+  writeFileSync(jsonFileName, JSON.stringify(accountsObject));
+};
+
+createAccounts()
+  .then((accountsObject: any) => console.log(accountsObject.accountList))
+  .catch(err => console.error(err));
