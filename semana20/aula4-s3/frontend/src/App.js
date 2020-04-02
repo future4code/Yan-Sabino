ac1628b8ee3f22f44cba05e58bf3df9973fee69d
Yan Sabino <yan.sabino@hotmail.com>
2020-04-02 14:43:52 -0300

    first commit

---
+++ semana20/aula4-s3/frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h4>Olá! Bora fazer uploads 😉</h4>
+      <form
+        action="https://wfdo9ou1yl.execute-api.us-east-1.amazonaws.com/v4/uploadFile"
+        method="POST"
+        enctype="multipart/form-data"
+      >
+        <input name="arquivo" type="file" />
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
