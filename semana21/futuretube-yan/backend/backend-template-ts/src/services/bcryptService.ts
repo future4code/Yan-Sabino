bb91488e81f2404fd89dc21fcf7622ed79cc7bac
Yan Sabino <yan.sabino@hotmail.com>
2020-04-28 14:25:23 -0300

    trying to encrypt the password with bcryptjs

--- semana21/futuretube-yan/backend/backend-template-ts/src/services/bcryptService.ts
+++ semana21/futuretube-yan/backend/backend-template-ts/src/services/bcryptService.ts
@@ -1,18 +1,19 @@
 import { CryptographyGateway } from "../business/gateways/cryptographyGateway";
-
+import * as bcrypt from "bcryptjs";
 
 export class BcryptService implements CryptographyGateway {
-  // private static BCRYPT_SALTS_ROUND = 10;
+  private static BCRYPT_SALTS_ROUND = 10;
+
 
   async encrypt(word: string): Promise<string> {
-    // const generatedSalt = await bcrypt.genSalt(
-    //   BcryptService.BCRYPT_SALTS_ROUND
-    // );
-    // const result = await bcrypt.hash(word, generatedSalt);
-    return word;
+    const generatedSalt = await bcrypt.genSalt(
+      BcryptService.BCRYPT_SALTS_ROUND
+    );
+    const result = await bcrypt.hash(word, generatedSalt);
+    return result;
   }
 
   async compare(word: string, hash: string): Promise<boolean> {
-    return word === hash
+    return bcrypt.compare(word, hash);
   }
 }
